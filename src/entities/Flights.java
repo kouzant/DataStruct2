8e45011f4d4faf06cafe96fd53228807c271420c
alafroiskiotos <praetorian@akyroi.gr>
2011-05-25 12:36:19 +0300

    Flights Entities

---
+++ src/entities/Flights.java
@@ -0,0 +1,47 @@
+package entities;
+
+import java.util.Date;
+
+public class Flights {
+	private String flightCode;
+	private Date arrivalTime;
+	private Date departureTime;
+	
+	public Flights(String flightCode, Date arrivalTime, Date departureTime){
+		this.flightCode=flightCode;
+		this.arrivalTime=arrivalTime;
+		this.departureTime=departureTime;
+	}
+
+	public String getFlightCode(){
+		return flightCode;
+	}
+	public Date getArrivalTime(){
+		return arrivalTime;
+	}
+	public Date getDepartureTime(){
+		return departureTime;
+	}
+	public void setFlightCode(String flightCode){
+		this.flightCode=flightCode;
+	}
+	public void setArrivalTime(Date arrivalTime){
+		this.arrivalTime=arrivalTime;
+	}
+	public void setDepartureTime(Date departureTime){
+		this.departureTime=departureTime;
+	}
+	@Override
+	public String toString(){
+		StringBuilder sb=new StringBuilder();
+		sb.append("\n");
+		sb.append("Flight Code: ");
+		sb.append(flightCode).append("\n");
+		sb.append("Departure Time: ");
+		sb.append(departureTime).append("\n");
+		sb.append("Arrival Time: ");
+		sb.append(arrivalTime).append("\n");
+		
+		return sb.toString();
+	}
+}
